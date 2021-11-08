60b686f6908363f3e0bacdcc8fed641ca1fd0524
Nathalie Möller <nathalie.moller@cea.fr>
2021-11-08 19:25:33 +0100

    Add norm2, axpy and copy.

--- plugins/ginkgo/src/converters/simplecsr_to_ginkgo_matrix.cpp
+++ plugins/ginkgo/src/converters/simplecsr_to_ginkgo_matrix.cpp
@@ -27,93 +27,93 @@
 
 #include <alien/ginkgo/backend.h>
 
-class SimpleCSR_to_Ginkgo_MatrixConverter : public Alien::IMatrixConverter {
-public:
-    SimpleCSR_to_Ginkgo_MatrixConverter() = default;
+class SimpleCSR_to_Ginkgo_MatrixConverter : public Alien::IMatrixConverter
+{
+ public:
+  SimpleCSR_to_Ginkgo_MatrixConverter() = default;
 
-    ~SimpleCSR_to_Ginkgo_MatrixConverter() override = default;
+  ~SimpleCSR_to_Ginkgo_MatrixConverter() override = default;
 
-public:
-    BackEndId sourceBackend() const override {
-        return Alien::AlgebraTraits<Alien::BackEnd::tag::simplecsr>::name();
-    }
+ public:
+  BackEndId sourceBackend() const override
+  {
+    return Alien::AlgebraTraits<Alien::BackEnd::tag::simplecsr>::name();
+  }
 
-    BackEndId targetBackend() const override {
-        return Alien::AlgebraTraits<Alien::BackEnd::tag::ginkgo>::name();
-    }
+  BackEndId targetBackend() const override
+  {
+    return Alien::AlgebraTraits<Alien::BackEnd::tag::ginkgo>::name();
+  }
 
-    void convert(const Alien::IMatrixImpl *sourceImpl,
-                 Alien::IMatrixImpl *targetImpl) const override;
+  void convert(const Alien::IMatrixImpl* sourceImpl,
+               Alien::IMatrixImpl* targetImpl) const override;
 
-    void _build(const Alien::SimpleCSRMatrix<Arccore::Real> &sourceImpl, Alien::Ginkgo::Matrix &targetImpl) const;
+  void _build(const Alien::SimpleCSRMatrix<Arccore::Real>& sourceImpl, Alien::Ginkgo::Matrix& targetImpl) const;
 
-    void _buildBlock(const Alien::SimpleCSRMatrix<Arccore::Real> &sourceImpl, Alien::Ginkgo::Matrix &targetImpl) const;
+  void _buildBlock(const Alien::SimpleCSRMatrix<Arccore::Real>& sourceImpl, Alien::Ginkgo::Matrix& targetImpl) const;
 };
 
-void SimpleCSR_to_Ginkgo_MatrixConverter::convert(const IMatrixImpl *sourceImpl, IMatrixImpl *targetImpl) const {
-    const auto &v = cast<Alien::SimpleCSRMatrix<Arccore::Real>>(sourceImpl, sourceBackend());
-    auto &v2 = cast<Alien::Ginkgo::Matrix>(targetImpl, targetBackend());
+void SimpleCSR_to_Ginkgo_MatrixConverter::convert(const IMatrixImpl* sourceImpl, IMatrixImpl* targetImpl) const
+{
+  const auto& v = cast<Alien::SimpleCSRMatrix<Arccore::Real>>(sourceImpl, sourceBackend());
+  auto& v2 = cast<Alien::Ginkgo::Matrix>(targetImpl, targetBackend());
 
-    alien_debug([&] {
-        cout() << "Converting Alien::SimpleCSRMatrix: " << &v << " to Ginkgo::Matrix " << &v2;
-    });
+  alien_debug([&] {
+    cout() << "Converting Alien::SimpleCSRMatrix: " << &v << " to Ginkgo::Matrix " << &v2;
+  });
 
-    if (targetImpl->block())
-        _buildBlock(v, v2);
-    else if (targetImpl->vblock())
-        throw Arccore::FatalErrorException(A_FUNCINFO, "Block sizes are variable - builds not yet implemented");
-    else
-        _build(v, v2);
+  if (targetImpl->block())
+    _buildBlock(v, v2);
+  else if (targetImpl->vblock())
+    throw Arccore::FatalErrorException(A_FUNCINFO, "Block sizes are variable - builds not yet implemented");
+  else
+    _build(v, v2);
 }
 
-void SimpleCSR_to_Ginkgo_MatrixConverter::_build(const Alien::SimpleCSRMatrix<Arccore::Real> &sourceImpl,
-                                                Alien::Ginkgo::Matrix &targetImpl) const {
+void SimpleCSR_to_Ginkgo_MatrixConverter::_build(const Alien::SimpleCSRMatrix<Arccore::Real>& sourceImpl,
+                                                 Alien::Ginkgo::Matrix& targetImpl) const
+{
 
-    const auto &dist = sourceImpl.distribution();
-    const auto &profile = sourceImpl.getCSRProfile();
+  const auto& dist = sourceImpl.distribution();
+  const auto& profile = sourceImpl.getCSRProfile();
 
-    // nb rows
-    const auto localSize = profile.getNRow();
+  // nb rows
+  const auto localSize = profile.getNRow();
 
-    const auto localOffset = dist.rowOffset();
+  const auto localOffset = dist.rowOffset();
 
-    const auto ilower = localOffset;
-    const auto iupper = localOffset + localSize - 1;
-    const auto jlower = ilower;
-    const auto jupper = iupper;
+  const auto ilower = localOffset;
+  const auto iupper = localOffset + localSize - 1;
+  const auto jlower = ilower;
+  const auto jupper = iupper;
 
-    // crée un tableau sizes, contenant pour chaque ligne, le nb nnz
-    /*auto sizes = Arccore::UniqueArray<int>(localSize);
-    for (auto row = 0; row < localSize; ++row) {
-        sizes[row] = profile.getRowSize(row);
-    }*/
+ 
 
-//    targetImpl.setProfile(ilower, iupper, jlower, jupper, sizes);
+  auto values = sourceImpl.internal().getValues();
+  auto cols = profile.getCols();
+  auto icount = 0;
 
-    auto values = sourceImpl.internal().getValues();
-    auto cols = profile.getCols();
-    auto icount = 0;
+  // for each row
+  for (auto irow = 0; irow < localSize; ++irow) {
+    const auto row = localOffset + irow;
 
-    // for each row
-    for (auto irow = 0; irow < localSize; ++irow) {
-        const auto row = localOffset + irow;
+    // get nb of values in this row
+    const auto ncols = profile.getRowSize(irow);
 
-        // get nb of values in this row
-        const auto ncols = profile.getRowSize(irow);
+    // set values in the target matrix, with the row id, and arrays of col ids and values
+    targetImpl.setRowValues(row, cols.subConstView(icount, ncols), values.subConstView(icount, ncols));
+    icount += ncols;
+  }
 
-        // set values in the target matrix, with the row id, and arrays of col ids and values
-        targetImpl.setRowValues(row, cols.subConstView(icount, ncols), values.subConstView(icount, ncols));
-        icount += ncols;
-    }
-
-    targetImpl.assemble();
+  targetImpl.assemble();
 }
 
-void SimpleCSR_to_Ginkgo_MatrixConverter::_buildBlock(const Alien::SimpleCSRMatrix<Arccore::Real> &sourceImpl,
-                                                     Alien::Ginkgo::Matrix &targetImpl) const {
+void SimpleCSR_to_Ginkgo_MatrixConverter::_buildBlock(const Alien::SimpleCSRMatrix<Arccore::Real>& sourceImpl,
+                                                      Alien::Ginkgo::Matrix& targetImpl) const
+{
 
-    
-/*
+
+  /*
   const auto& dist = sourceImpl.distribution();
   const auto& profile = sourceImpl.getCSRProfile();
   const auto localSize = profile.getNRow();
