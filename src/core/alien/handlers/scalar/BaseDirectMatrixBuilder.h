726fc1c6cf6ebac4d4a159cb51ffa203b2847c5b
Cédric Chevalier <cedric.chevalier@cea.fr>
2023-05-17 15:06:36 +0200

    [core]: some cleaning

--- src/core/alien/handlers/scalar/BaseDirectMatrixBuilder.h
+++ src/core/alien/handlers/scalar/BaseDirectMatrixBuilder.h
@@ -36,159 +36,169 @@
 
 #endif /* USE_VMAP */
 
-namespace Arccore {
-    class ITraceMng;
-    namespace MessagePassing {
-        class IMessagePassingMng;
-    }
+namespace Arccore
+{
+class ITraceMng;
+namespace MessagePassing
+{
+  class IMessagePassingMng;
+}
 } // namespace Arccore
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-namespace Alien {
+namespace Alien
+{
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-    template<typename Scalar>
-    class SimpleCSRMatrix;
+template <typename Scalar>
+class SimpleCSRMatrix;
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-    struct DirectMatrixOptions {
-        enum ResetFlag {
-            eNoReset,
-            eResetValues,
-            eResetProfile,
-            eResetAllocation
-        };
-        enum ReserveFlag {
-            eResetReservation,
-            eExtendReservation
-        };
-        enum SymmetricFlag {
-            eSymmetric,
-            eUnSymmetric
-        };
-    };
+struct DirectMatrixOptions
+{
+  enum ResetFlag
+  {
+    eNoReset,
+    eResetValues,
+    eResetProfile,
+    eResetAllocation
+  };
+  enum ReserveFlag
+  {
+    eResetReservation,
+    eExtendReservation
+  };
+  enum SymmetricFlag
+  {
+    eSymmetric,
+    eUnSymmetric
+  };
+};
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-    namespace Common {
+namespace Common
+{
 
-        /*---------------------------------------------------------------------------*/
-        /*---------------------------------------------------------------------------*/
+  /*---------------------------------------------------------------------------*/
+  /*---------------------------------------------------------------------------*/
 
-        class ALIEN_EXPORT DirectMatrixBuilder {
-        public:
-            using ResetFlag = DirectMatrixOptions::ResetFlag;
-            using ReserveFlag = DirectMatrixOptions::ReserveFlag;
-            using SymmetricFlag = DirectMatrixOptions::SymmetricFlag;
+  class ALIEN_EXPORT DirectMatrixBuilder
+  {
+   public:
+    using ResetFlag = DirectMatrixOptions::ResetFlag;
+    using ReserveFlag = DirectMatrixOptions::ReserveFlag;
+    using SymmetricFlag = DirectMatrixOptions::SymmetricFlag;
 
-        public:
-            using MatrixElement = MatrixElementT<DirectMatrixBuilder>;
+   public:
+    using MatrixElement = MatrixElementT<DirectMatrixBuilder>;
 
-        public:
-            DirectMatrixBuilder(IMatrix &matrix, ResetFlag reset_flag,
-                                SymmetricFlag symmetric_flag = SymmetricFlag::eSymmetric);
+   public:
+    DirectMatrixBuilder(IMatrix& matrix, ResetFlag reset_flag,
+                        SymmetricFlag symmetric_flag = SymmetricFlag::eSymmetric);
 
-            virtual ~DirectMatrixBuilder();
+    virtual ~DirectMatrixBuilder();
 
-            DirectMatrixBuilder(DirectMatrixBuilder &) = delete;
+    DirectMatrixBuilder(DirectMatrixBuilder&) = delete;
 
-            DirectMatrixBuilder(DirectMatrixBuilder &&) = delete;
+    DirectMatrixBuilder(DirectMatrixBuilder&&) = delete;
 
-            DirectMatrixBuilder &operator=(const DirectMatrixBuilder &) = delete;
+    DirectMatrixBuilder& operator=(const DirectMatrixBuilder&) = delete;
 
-            DirectMatrixBuilder &operator=(DirectMatrixBuilder &&) = delete;
+    DirectMatrixBuilder& operator=(DirectMatrixBuilder&&) = delete;
 
-        public:
-            MatrixElement operator()(const Integer iIndex, const Integer jIndex) {
-                return MatrixElement(iIndex, jIndex, *this);
-            }
+   public:
+    MatrixElement operator()(const Integer iIndex, const Integer jIndex)
+    {
+      return MatrixElement(iIndex, jIndex, *this);
+    }
 
-            void reserve(Arccore::Integer n, ReserveFlag flag = ReserveFlag::eResetReservation);
+    void reserve(Arccore::Integer n, ReserveFlag flag = ReserveFlag::eResetReservation);
 
-            void reserve(Arccore::ConstArrayView<Arccore::Integer> indices, Arccore::Integer n,
-                         ReserveFlag flag = ReserveFlag::eResetReservation);
+    void reserve(Arccore::ConstArrayView<Arccore::Integer> indices, Arccore::Integer n,
+                 ReserveFlag flag = ReserveFlag::eResetReservation);
 
-            void allocate();
+    void allocate();
 
-            void addData(Arccore::Integer iIndex, Arccore::Integer jIndex, Arccore::Real value);
+    void addData(Arccore::Integer iIndex, Arccore::Integer jIndex, Arccore::Real value);
 
-            void addData(Arccore::Integer iIndex, Arccore::Real factor,
-                         Arccore::ConstArrayView<Arccore::Integer> jIndexes,
-                         Arccore::ConstArrayView<Arccore::Real> jValues);
+    void addData(Arccore::Integer iIndex, Arccore::Real factor,
+                 Arccore::ConstArrayView<Arccore::Integer> jIndexes,
+                 Arccore::ConstArrayView<Arccore::Real> jValues);
 
-            void setData(Arccore::Integer iIndex, Arccore::Integer jIndex, Arccore::Real value);
+    void setData(Arccore::Integer iIndex, Arccore::Integer jIndex, Arccore::Real value);
 
-            void setData(Arccore::Integer iIndex, Arccore::Real factor,
-                         Arccore::ConstArrayView<Arccore::Integer> jIndexes,
-                         Arccore::ConstArrayView<Arccore::Real> jValues);
+    void setData(Arccore::Integer iIndex, Arccore::Real factor,
+                 Arccore::ConstArrayView<Arccore::Integer> jIndexes,
+                 Arccore::ConstArrayView<Arccore::Real> jValues);
 
-            void finalize();
+    void finalize();
 
-            void squeeze();
+    void squeeze();
 
-            [[nodiscard]] Arccore::String stats() const;
+    [[nodiscard]] Arccore::String stats() const;
 
-            [[nodiscard]] Arccore::String stats(Arccore::IntegerConstArrayView ids) const;
+    [[nodiscard]] Arccore::String stats(Arccore::IntegerConstArrayView ids) const;
 
-        protected:
-            IMatrix &m_matrix;
+   protected:
+    IMatrix& m_matrix;
 
-            SimpleCSRMatrix<Real> *m_matrix_impl;
+    SimpleCSRMatrix<Real>* m_matrix_impl;
 
-            Integer m_local_offset, m_global_size, m_local_size;
-            Integer m_col_global_size;
-            ArrayView<Integer> m_row_starts;
-            ArrayView<Integer> m_cols;
-            ArrayView<Real> m_values;
-            UniqueArray<Integer> m_row_sizes;
+    Integer m_local_offset, m_global_size, m_local_size;
+    Integer m_col_global_size;
+    ArrayView<Integer> m_row_starts;
+    ArrayView<Integer> m_cols;
+    ArrayView<Real> m_values;
+    UniqueArray<Integer> m_row_sizes;
 
-            ResetFlag m_reset_flag;
-            bool m_allocated;
-            bool m_finalized;
-            bool m_symmetric_profile;
+    ResetFlag m_reset_flag;
+    bool m_allocated;
+    bool m_finalized;
+    bool m_symmetric_profile;
 
-            Integer m_nproc;
-            IMessagePassingMng *m_parallel_mng;
-            ITraceMng *m_trace;
+    Integer m_nproc;
+    IMessagePassingMng* m_parallel_mng;
+    ITraceMng* m_trace;
 
 #ifdef USE_VMAP
-            typedef VMap<Integer, Real> ColValueData;
+    typedef VMap<Integer, Real> ColValueData;
 #else /* USE_VMAP */
-            typedef std::map<Integer, Real> ColValueData;
+    typedef std::map<Integer, Real> ColValueData;
 #endif /* USE_VMAP */
-            typedef std::map<Integer, ColValueData> ExtraRows;
-            ExtraRows m_extras;
+    typedef std::map<Integer, ColValueData> ExtraRows;
+    ExtraRows m_extras;
 
-        private:
-            void computeProfile(Arccore::ConstArrayView<Arccore::Integer> sizes);
+   private:
+    void computeProfile(Arccore::ConstArrayView<Arccore::Integer> sizes);
 
-            void updateProfile(Arccore::UniqueArray<Arccore::Integer> &row_starts,
-                               Arccore::UniqueArray<Arccore::Integer> &cols,
-                               Arccore::UniqueArray<Arccore::Real> &values);
+    void updateProfile(Arccore::UniqueArray<Arccore::Integer>& row_starts,
+                       Arccore::UniqueArray<Arccore::Integer>& cols,
+                       Arccore::UniqueArray<Arccore::Real>& values);
 
-            class IndexEnumerator;
+    class IndexEnumerator;
 
-            class FullEnumerator;
+    class FullEnumerator;
 
-            template<typename Enumerator>
-            void _stats(std::ostream &o, const Enumerator &e) const;
+    template <typename Enumerator>
+    void _stats(std::ostream& o, const Enumerator& e) const;
 
-            void _startTimer() {}
+    void _startTimer() {}
 
-            void _stopTimer() {}
-        };
+    void _stopTimer() {}
+  };
 
-        /*---------------------------------------------------------------------------*/
-        /*---------------------------------------------------------------------------*/
+  /*---------------------------------------------------------------------------*/
+  /*---------------------------------------------------------------------------*/
 
-    } // namespace Common
+} // namespace Common
 
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
